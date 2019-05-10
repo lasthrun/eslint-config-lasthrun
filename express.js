addc01b21b50bb57c8ac5adba4ac069a6f357246
lasthrun <dev.lasthrun@gmail.com>
2019-05-10 23:49:33 +0800

    Initial commit

---
+++ express.js
@@ -0,0 +1,37 @@
+const airbnbBaseStyleRules = require('eslint-config-airbnb-base/rules/style').rules;
+const airbnbBaseVariablesRules = require('eslint-config-airbnb-base/rules/variables').rules;
+
+module.exports = {
+  'parser': '@typescript-eslint/parser',
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'extends': [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'rules': {
+    '@typescript-eslint/camelcase': airbnbBaseStyleRules.camelcase,
+    '@typescript-eslint/indent': airbnbBaseStyleRules.indent,
+    '@typescript-eslint/no-array-constructor': airbnbBaseStyleRules['no-array-constructor'],
+    '@typescript-eslint/no-unused-vars': airbnbBaseVariablesRules['no-unused-vars'],
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.spec.*"]}],
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': [
+          '.ts',
+        ],
+      },
+    },
+    "import/core-modules": [ "ember" ]
+  },
+  'env': {
+    'browser': true,
+    'node': true,
+    'jest': true,
+    'commonjs': true,
+    'es6': true,
+  },
+};
