16c2fb820a30295298ce52ca2fdf0b1a3eb51238
lasthrun <dev.lasthrun@gmail.com>
2020-05-28 23:57:28 +0800

    add react config

---
+++ react.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'extends': [
+    'airbnb-typescript'
+  ],
+  'rules': {
+    'import/prefer-default-export': ['off'],
+  },
+  'env': {
+    'browser': true,
+    'jest': true,
+    'commonjs': true,
+    'es2020': true,
+  },
+};
